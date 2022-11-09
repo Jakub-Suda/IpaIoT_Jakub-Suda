84c698575267330466458aa57a7d2eed0a5b2825
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-11-09 20:07:54 +0100

    update

--- index.php
+++ index.php
@@ -1,6 +1,20 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>3D tlačiarne</title>
+    <link rel="stylesheet" href="/style.css" media="screen">
+</head>
+<body class="grid-container">
+
 <?php 
 echo '<h1>HTML again</h1>';
 
 echo '<p>First web on Azure</p>'; 
 
 ?>
+
+</body>
+
+
