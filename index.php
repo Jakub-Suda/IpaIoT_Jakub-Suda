a8042405536e436aa671b5516b9ec1d66996de1e
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-11-09 20:08:48 +0100

    repair

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3D tlačiarne</title>
+    <title>PHP_thing</title>
     <link rel="stylesheet" href="/style.css" media="screen">
 </head>
 <body class="grid-container">
@@ -11,7 +11,7 @@
 <?php 
 echo '<h1>HTML again</h1>';
 
-echo '<p>First web on Azure</p>'; 
+echo '<p>First web on Azure + css</p>'; 
 
 ?>
 
