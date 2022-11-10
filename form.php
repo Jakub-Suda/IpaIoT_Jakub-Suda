d5268777be38c629f22b0e7e9cf1bf85f2070fd3
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-11-10 09:44:34 +0100

    form.php

---
+++ form.php
@@ -0,0 +1,50 @@
+<form id="Questions" >
+        <label for="name">Meno:</label><br>
+        <input id="name" type="text" name="username" required><br>
+
+        <label for="email">Email:</label><br>
+        <input id="email" type="email" name="username" required><br>
+
+        <label for="pic" >Príloha:</label> <br>
+        <input type="url" name="url" id="pic" size="100"> <br>
+
+        Vlastníte 3D tlačiareň ?<br>
+        <ul>
+            <li><input type="radio" id ="YES"  name="printer"  value="Ano"  > <label for="YES">Áno</label></li>
+            <li><input type="radio" id ="NO"  name="printer"  value="nie"  > <label for="NO">Nie</label></li>
+        </ul>
+        <br>
+
+        3D tlaci sa venujete?:<br>
+        <ul>
+            <li><input type="checkbox" id="praca" name="praca" value="praca">
+                <label for="praca"> V práci</label></li>
+            <li><input type="checkbox" id="doma" name="doma" value="doma">
+                <label for="doma"> Doma</label></li>
+            <li><input type="checkbox" id="studujem" name="studujem" value="studujem">
+                <label for="studujem"> Študujem to</label></li>
+        </ul>
+        <br>
+
+        <label class="long" for="tlacenie">Ako často tlačíťe na 3D tlačiarni ?</label><br>
+        <input type="text" name="sposob" id="tlacenie" size="15"
+               title="40 znakov max" maxlength="40"
+               spellcheck="true" lang="sk"
+               list="intervals"><br><br>
+    
+        <datalist id="intervals">
+            <option>Každý deň</option>
+            <option>Niekoľko raz do týždňa</option>
+            <option>Raz za pár týždňov</option>
+            <option>Vôbec</option>
+        </datalist>
+
+        <label for="opinion">Názor/Otázka</label><br>
+        <textarea name="Text" id="opinion"
+                  lang="sk" required
+                  cols="100" rows="5"></textarea>
+        <br>
+        <br>
+        <input type="reset" value="reset" >
+        <input type="submit" value="submit" >
+        </form>
