c7090a5d738ff9905fd2de9af0c309eeacb6b400
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2022-11-10 09:27:23 +0100

    index4.php update

--- index4.php
+++ index4.php
@@ -1,6 +1,6 @@
 <?php
-        echo '<h1>getParameters page</h1>';
-        
+        echo '<h1>index4.php web page with parameters</h1>';
+        echo '<h2>https://sudiplatform.azurewebsites.net/index4.php?a=10&b=2</h2>';
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
         
