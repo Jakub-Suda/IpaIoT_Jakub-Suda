63218097a7d2bb0a619bc88e82d76bfbc0f7dbfe
Jakub-Suda <114649891+Jakub-Suda@users.noreply.github.com>
2023-01-12 09:26:02 +0100

    12

--- skuska.php
+++ skuska.php
@@ -13,7 +13,7 @@
             <label for="opinion">Input string</label><br>
             <textarea name="Text" id="opinion"
                     lang="sk"
-                    cols="75" rows="5"> This is IPaIoT final exam I've been preparing for. The exam is easy and teacher is great.</textarea>
+                    cols="75" rows="5">This is IPaIoT final exam I've been preparing for. The exam is easy and teacher is great.</textarea>
             <br>
             <input type="reset" value="reset" >
             <input type="submit" value="submit" >
